723d14c158ecd687d128da068f236b10d26dfd07
ATT <ATT@mail.com>
2018-04-18 14:16:41 +0000

    json

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -3,14 +3,14 @@ import { Product } from '../domain/product';
 
 @Injectable()
 export class ProductsListService {
-
+//private url='http://localhost:3000/posts';
   constructor() { }
-
+data: Product[] = [
+  {code: 'p100', nom: 'coffee', prix: 1500.0},
+  {code: 'p200', nom: 'vue sur mer', prix: 500.0},
+  {code: 'p300', nom: 'vue sur terre', prix: 100.0}
+];
   getAllProducts(): Product[] {
-   return [
-      {code: 'p100', nom: 'coffee', prix: 1500.0},
-      {code: 'p200', nom: 'vue sur mer', prix: 500.0},
-      {code: 'p300', nom: 'vue sur terre', prix: 100.0}
-    ];
+   return this.data;
      }
   }
